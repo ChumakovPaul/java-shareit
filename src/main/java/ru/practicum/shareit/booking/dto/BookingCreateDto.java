18c521d493243e23de35290aae895d201140b2ac
Paul Chumakov <paul.chumakof@yandex.ru>
2025-07-16 12:25:52 +0300

    Выполнить фз 15 спринта

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.booking.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class BookingCreateDto {
+    @NotNull
+    private Long itemId;
+    @NotNull
+    @FutureOrPresent
+    private LocalDateTime start;
+    @NotNull
+    @Future
+    private LocalDateTime end;
+    private Long bookerId;
+
+    @AssertTrue
+    boolean isStartBeforeEnd() {
+        return start.isBefore(end);
+    }
+}
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,4 +1,26 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ru.practicum.shareit.booking.Status;
+import ru.practicum.shareit.item.dto.ItemBookingDto;
+import ru.practicum.shareit.user.dto.UserBookerDto;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class BookingDto {
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private ItemBookingDto item;
+    private UserBookerDto booker;
+//    private long bookerId;
+    private Status status;
 }
